a54f33796303f6271d184c08d44c76f44781f4fc
Francisco Ignácio Fontoura Solla <francisco.solla@gmail.com>
2025-05-03 23:47:39 -0300

    Update Metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center p-2 gap-8">
+    <div className="flex flex-col items-center p-2 gap-8 max-w-3xl m-auto">
       <header className="flex flex-col items-center">
         <div className="rounded-full overflow-hidden w-fit aspect-square">
           <Image
@@ -42,21 +42,23 @@ export default function Home() {
       </header>
       <p className="flex flex-col gap-2">
         <span>
-          I’m a Senior Software Engineer with 8+ years of experience building
-          fast, accessible, and scalable applications across SaaS, marketplaces,
-          and high-growth platforms. My core stack includes React, Next.js,
-          TypeScript, and modern testing frameworks — and I’ve led projects
-          ranging from billing systems to design system overhauls and Core Web
-          Vitals optimization.
+          I’m a Senior Software Engineer with{" "}
+          <strong>8+ years of experience</strong> building fast, accessible, and
+          scalable applications across SaaS, marketplaces, and high-growth
+          platforms. My core stack includes{" "}
+          <strong>React, Next.js, React Native, Expo, TypeScript</strong>, and
+          modern testing frameworks — and I’ve led projects ranging from billing
+          systems to design system overhauls and Core Web Vitals optimization.
         </span>
         <span>
-          In recent years, I’ve grown increasingly drawn to Web3 — not just as a
-          new technical domain, but as a meaningful evolution in how we build
-          digital systems. I see blockchain as a foundation for trust,
-          transparency, and user ownership — technologies that can challenge
-          legacy financial structures and unlock new models of coordination.
-          This shift isn’t just about staying ahead technically; it’s about
-          contributing to a more open, resilient internet.
+          In recent years, I’ve grown increasingly drawn to{" "}
+          <strong>Web3</strong> — not just as a new technical domain, but as a
+          meaningful evolution in how we build digital systems. I see blockchain
+          as a foundation for trust, transparency, and user ownership —
+          technologies that can challenge legacy financial structures and unlock
+          new models of coordination. This shift isn’t just about staying ahead
+          technically; it’s about contributing to a more open, resilient
+          internet.
         </span>
         <span>
           I’m now focused on applying my software engineering expertise and
